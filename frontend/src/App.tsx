cfe55d3d751dedabdfd9b8945d8e8d0d2ef1370b
JozefTkocz <joe@bitbloom.tech>
2024-09-27 15:36:22 +0100

    fix cli command

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Deployed from CI!</h1>
+      <h1>Proof that CI works</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
