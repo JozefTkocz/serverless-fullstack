beb63c602937722513c2d7d4570e475c9bc4f54c
JozefTkocz <joe@bitbloom.tech>
2024-10-12 22:20:18 +0100

    add new file

---
+++ frontend/src/components/DebouncedButton.tsx
@@ -0,0 +1,30 @@
+import { Button, ButtonProps, CircularProgress } from "@mui/material";
+import React, { ReactNode, useState } from "react";
+
+export function DebouncedButton({
+  children,
+  ...props
+}: ButtonProps & { children: ReactNode }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { onClick } = props;
+  const handleClick = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setIsLoading(true);
+    if (onClick) {
+      await onClick(event);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      {!isLoading ? (
+        <Button {...{ ...props, onClick: handleClick }}>{children}</Button>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
+}
