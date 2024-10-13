aad544a695a37219c504b8807dc955e4b6360499
JozefTkocz <joe@bitbloom.tech>
2024-10-13 21:37:11 +0100

    add untracked files

---
+++ frontend/src/routes/index.lazy.tsx
@@ -0,0 +1,13 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3>Tumpr 2.0</h3>
+    </div>
+  );
+}
