9a4a5be01b1886fa03ccf28a1ec1041f3f16beaa
JozefTkocz <joe@bitbloom.tech>
2024-11-22 22:24:42 +0000

    log middleware

--- lambdas/backend_api/middleware/auth.py
+++ lambdas/backend_api/middleware/auth.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import Callable
 
 from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 from aws_lambda_powertools.utilities.typing import LambdaContext
@@ -7,25 +7,22 @@ from aws_lambda_powertools.event_handler.exceptions import (
     UnauthorizedError,
 )
 
-from dynamodb.users import User
 from services.auth import AuthTokenService
 
 from config import users_table
-
-
-class AuthenticatedHandler(Protocol):
-    def __call__(self, current_user: User, *args, **kwargs) -> dict: ...
 
 
 # Middleware to check the user is authenticated and provide the current user
 # to the endpoint
 @lambda_handler_decorator
 def authenticated_user(
-    handler: AuthenticatedHandler,
+    handler: Callable[[dict, LambdaContext], dict],
     # Will be the lambda URL API call event
     event: dict,
-    _: LambdaContext,
+    context: LambdaContext,
 ) -> dict:
+    print("========")
+    print("in middleware")
     headers: dict[str, str] = event["headers"]
 
     if not (token_string := headers.get("auth_token")):
@@ -37,5 +34,6 @@ def authenticated_user(
     if not current_user:
         raise UnauthorizedError("Unauthorized")
 
+    print(current_user)
     event["current_user"] = current_user
-    return handler(current_user)
+    return handler(event, context)
