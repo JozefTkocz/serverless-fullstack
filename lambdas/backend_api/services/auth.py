4ddb6035665a025ef3e3a69b7f3a0bb8dfd786ac
JozefTkocz <joe@bitbloom.tech>
2024-11-22 21:32:41 +0000

    fix imports

---
+++ lambdas/backend_api/services/auth.py
@@ -0,0 +1,35 @@
+import jwt
+from pydantic import BaseModel
+
+from config import dynamic_config
+
+
+class SessionToken(BaseModel):
+    """
+    Data encoded into the JWT -not accessible to the user
+    """
+
+    email: str
+    auth_token: str
+
+
+class SessionInfo(BaseModel):
+    """
+    Data sent back to the user after authenticating
+    """
+
+    email: str
+    message: str = ""
+    token_expires: int | None = None
+
+
+class AuthTokenService:
+    @staticmethod
+    def encode_token(token: SessionToken) -> str:
+        return jwt.encode(
+            token.model_dump(), dynamic_config.jwt_secret, algorithm="HS256"
+        )
+
+    @staticmethod
+    def decode_token(token_str: str) -> SessionToken:
+        return jwt.decode(token_str, dynamic_config.jwt_secret, algorithms=["HS256"])
